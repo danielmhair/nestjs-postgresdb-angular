67cfc8af4f7d61d94c9e795d69ea0a06ecc78607
Daniel Hair <danielmhair@gmail.com>
2023-02-11 17:19:26 +0000

    chore: update codespaces config

---
+++ .devcontainer/sql-setup.sh
@@ -0,0 +1,5 @@
+sudo apt-get install wget ca-certificates -y
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+sudo apt-get update
+sudo apt-get install postgresql postgresql-contrib -y
